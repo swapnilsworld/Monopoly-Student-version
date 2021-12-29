62483c429afb1619ec64a9625c6a9eee370d7931
swapnilsworld <45210958+swapnilsworld@users.noreply.github.com>
2021-12-29 22:41:33 +0530

    Initial commit

---
+++ src/test/java/MonopolyTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+class MonopolyTest {
+    Monopoly player = new Monopoly(0);
+
+    @Test
+    public void when_die_rolls_are_1_and_2_current_position_should_advance_to_3(){
+        //WRITE CODE HERE
+    }
+    @Test
+    public void when_die_rolls_are_3_and_3_advance_should_be_called_again_and_when_1_and_3_are_rolled_current_position_should_be_updated_to_10(){
+        // WRITE CODE HERE
+    }
+
+}
