ff643746997f57794fcee35bc78b863c052db039
swapnilsworld <45210958+swapnilsworld@users.noreply.github.com>
2021-12-29 22:51:29 +0530

    assignment submission

--- src/test/java/MonopolyTest.java
+++ src/test/java/MonopolyTest.java
@@ -13,10 +13,17 @@ class MonopolyTest {
     @Test
     public void when_die_rolls_are_1_and_2_current_position_should_advance_to_3(){
         //WRITE CODE HERE
+
+        Monopoly spiedPlayer = Mockito.spy(player);
+        Mockito.when(spiedPlayer.throwDie()).thenReturn(1,2);
+        assertEquals(3,spiedPlayer.advance());
     }
     @Test
     public void when_die_rolls_are_3_and_3_advance_should_be_called_again_and_when_1_and_3_are_rolled_current_position_should_be_updated_to_10(){
         // WRITE CODE HERE
+        Monopoly spiedPlayer = Mockito.spy(player);
+        Mockito.when(spiedPlayer.throwDie()).thenReturn(3,3,1,3);
+        assertEquals(10,spiedPlayer.advance());
     }
 
 }
